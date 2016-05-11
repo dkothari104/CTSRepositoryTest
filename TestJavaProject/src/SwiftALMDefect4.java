42d8430f2470a3927114aacbb0d22babf19b36cc
dkothari104 <dkothari@digite.com>
2016-05-11 15:55:16 +0530

    IssueID::DEF4 Reviewer::admin Status::Done Comments::test

--- TestJavaProject/src/SwiftALMDefect4.java
+++ TestJavaProject/src/SwiftALMDefect4.java
@@ -3,6 +3,6 @@ public class SwiftALMDefect4
 {
 	public static void main(String[] args)
 	{
-		System.out.println("This is a Defect with ID DEF4.");
+		System.out.println("This is a Defect with ID DEF4.......");
 	}
 }
