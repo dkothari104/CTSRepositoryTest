e0f4ae3b6d4b7868afa408830c7e2d1cc6c650db
dkothari104 <dkothari@digite.com>
2016-05-11 18:16:20 +0530

    IssueID:DEFF1 Reviewer::admin Status::Done Comments::hooks validaton

--- TestJavaProject/src/SwiftALMUserStory7.java
+++ TestJavaProject/src/SwiftALMUserStory7.java
@@ -3,7 +3,7 @@ public class SwiftALMUserStory7 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("This is a User Story with ID UST7........");
+		System.out.println("chnages|This is a User Story with ID UST7........");
 		System.out.println("This is a User Story with ID UST7........");
 	}
 
