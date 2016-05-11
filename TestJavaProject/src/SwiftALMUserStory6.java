a62bb37ed6774a4ce031d7a1b85702d10ea7e3a2
dkothari104 <dkothari@digite.com>
2016-05-11 14:33:42 +0530

    IssueID::Ust8 Reviewer::admin Status::Done Comments::test

--- TestJavaProject/src/SwiftALMUserStory6.java
+++ TestJavaProject/src/SwiftALMUserStory6.java
@@ -3,7 +3,7 @@ public class SwiftALMUserStory6 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("This is User Story 6.");
+		System.out.println("This is User Story 6 with ID UST6.");
 	}
 
 }
