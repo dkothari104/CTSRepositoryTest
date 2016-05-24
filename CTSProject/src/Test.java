1d02c1825f3b09c51f8008bf84dc9f12dbc76cd1
dkothari104 <dkothari@digite.com>
2016-05-24 16:47:32 +0530

    IssueId::DEF1 OwnerId::HMSS000038CTSDEMO entityType::Dft Reviewer::admin Status::Done Comments::The Defect has been successfully implemented.

--- CTSProject/src/Test.java
+++ CTSProject/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) 
 	{
-	System.out.println("Testing once again");
+	System.out.println("Testing once again and again");
 	System.out.println("Testing once again");
 	System.out.println("Testing once again and again");
 	}
