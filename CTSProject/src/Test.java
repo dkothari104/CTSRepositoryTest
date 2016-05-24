5bedde140d5f9259a3fc1b23ea8011057339c694
dkothari104 <dkothari@digite.com>
2016-05-24 15:44:28 +0530

    IssueId::UST1 OwnerId::HMSS000038CTSDEMO entityType::AUSRST Reviewer::admin Status::Done Comments::The UserStory has been successfully implemented.

--- CTSProject/src/Test.java
+++ CTSProject/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) 
 	{
-	System.out.println("Testing");
+	System.out.println("Testing once again");
 	System.out.println("Testing once again");
 	System.out.println("Testing once again");
 	}
