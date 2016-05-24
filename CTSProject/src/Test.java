11fff5153e9910cd07b2a083aac876e10932c044
dkothari104 <dkothari@digite.com>
2016-05-24 15:48:36 +0530

    IssueId::UST1 OwnerId::HMSS000038CTSDEMO entityType::AUSRST Reviewer::admin Status::Done Comments::The UserStory has been implemented again.

--- CTSProject/src/Test.java
+++ CTSProject/src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	{
 	System.out.println("Testing once again");
 	System.out.println("Testing once again");
-	System.out.println("Testing once again");
+	System.out.println("Testing once again and again");
 	}
 
 }
