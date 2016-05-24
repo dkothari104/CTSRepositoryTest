4cda684bbf785df70df7fd828abdeaf14ca5244a
dkothari104 <dkothari@digite.com>
2016-05-24 16:52:41 +0530

    IssueId::DEF1 OwnerId::HMSS000038CTSDEMO entityType::Dft Reviewer::admin Status::Done Comments::The Defect has been implemented once again.

--- CTSProject/src/Test.java
+++ CTSProject/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) 
 	{
 	System.out.println("Testing once again and again");
-	System.out.println("Testing once again");
+	System.out.println("Testing once again and again");
 	System.out.println("Testing once again and again");
 	}
 
