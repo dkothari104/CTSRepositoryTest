6c762cc28cfbb85cd2172dadc5b0c247f05943eb
dkothari104 <dkothari@digite.com>
2016-05-27 12:06:33 +0530

    IssueId::DEF2 OwnerId::HMSS000038CTSDEMO entityType::Dft Reviewer::admin Status::OK Comments::Test

--- CTSProject/src/Test.java
+++ CTSProject/src/Test.java
@@ -6,6 +6,7 @@ public class Test {
 	System.out.println("Testing once again and again");
 	System.out.println("Testing once again and again");
 	System.out.println("Testing once again and again");
+	//testcommit
 	}
 
 }
