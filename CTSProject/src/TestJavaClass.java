b692cec913f9f5ceb17d66df43b45cde8443d77e
dkothari104 <dkothari@digite.com>
2016-06-07 17:52:29 +0530

    IssueId::UST1 OwnerId::HMSS000038CTSDEMO entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- CTSProject/src/TestJavaClass.java
+++ CTSProject/src/TestJavaClass.java
@@ -3,7 +3,7 @@ public class TestJavaClass {
 	
 public static void main(String[] args) {
 
-	System.out.println("This is commit against UST8.");
+	System.out.println("This is commit against UST8123");
 }
 
 }
