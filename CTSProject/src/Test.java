64de4bacdcf78002f0a4391b5d6c1ccda2ce6878
dkothari104 <dkothari@digite.com>
2016-06-28 11:10:10 +0530

    IssueId::UST3 OwnerId::HMSS00004322 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- CTSProject/src/Test.java
+++ CTSProject/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) 
 	{
 	System.out.println("Testing once again and again and again ek once more aur baar");
-	System.out.println("Testing once again and again and again and once again");
+	System.out.println("Testing once again and again and  and again and once again");
 	System.out.println("Testing once again and again and again and ag aur ek baarain");
 	//testcommit
 	}
