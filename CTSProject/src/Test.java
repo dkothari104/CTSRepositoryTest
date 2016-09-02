97ffdd08c08405b4724b7dab13d68714d925cc5e
dkothari104 <dkothari@digite.com>
2016-09-02 12:48:00 +0530

    IssueId::UST1 OwnerId::HMSS00004927 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- CTSProject/src/Test.java
+++ CTSProject/src/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	System.out.println("Testing once again and again and again ek once more aur baar");
 	System.out.println("Testing once again aoknd again and  and again and once again");
 	System.out.println("Testin aur ek hcwduv once again and again and again and ag aur ek baarain");
-	//testcommit
+	//testcommitVSDAHV
 	}
 
 }
